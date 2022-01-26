94d670e947e9c170b7e8e807653994556ea24f93
James <56299665+JamesCS123@users.noreply.github.com>
2022-01-26 19:07:01 +0200

    Add validation to Personal Details Form

--- src/app/pages/dashboard/account-settings/personal-details-form/personal-details-form.component.ts
+++ src/app/pages/dashboard/account-settings/personal-details-form/personal-details-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-personal-details-form',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonalDetailsFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  submitted = false;
+  loading = false;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { 
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(500)]],
+      surname: ['', [Validators.required, Validators.maxLength(500)]],
+      email: ['', [Validators.required, Validators.email, Validators.maxLength(500)]]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  submit() {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+    else {
+      this.loading = true;
+    }
   }
 
 }
