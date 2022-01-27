2979845706cee0a8263d5fc52812f43bf088026b
James <56299665+JamesCS123@users.noreply.github.com>
2022-01-27 14:32:26 +0200

    Add validation to change-password-form

--- src/app/pages/dashboard/account-settings/change-password-form/change-password-form.component.ts
+++ src/app/pages/dashboard/account-settings/change-password-form/change-password-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators, FormGroupDirective, NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-change-password-form',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  submitted = false;
+  loading = false;
+
+  constructor(public formBuilder: FormBuilder) { 
+    this.form = this.formBuilder.group({
+      currentPassword: ['', Validators.required],
+      newPassword: ['', [Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')]],
+      confirmPassword: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  submit() {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+    else {
+      this.loading = true;
+    }
   }
 
 }
